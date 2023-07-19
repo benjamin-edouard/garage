f2aa6d774cc573716b2414491ac780f54f33c247
benjamin-edouard <130186004+benjamin-edouard@users.noreply.github.com>
2023-07-19 11:10:56 +0200

    Worked on the login authentification

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,7 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\CarsAd;
 use App\Entity\Hours;
-use App\Entity\User;
+//use App\Entity\User;
+use App\Entity\Users;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -22,7 +23,7 @@ class DashboardController extends AbstractDashboardController
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
+        return $this->redirect($adminUrlGenerator->setController(CarsAdCrudController::class)->generateUrl());
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
@@ -44,9 +45,10 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Utilisateur', 'fa fa-user-o', User::class);
+        //yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-user-o', Users::class)->setPermission('ROLE_ADMIN');
         yield MenuItem::linkToCrud('Annonces', 'fas fa-list', CarsAd::class);
         yield MenuItem::linkToCrud('Horaires', 'fa fa-clock', Hours::class);
+        yield MenuItem::linkToLogout('Deconnexion', 'fa fa-sign-out');
     }
 }
